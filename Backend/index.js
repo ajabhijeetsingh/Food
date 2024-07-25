3323d4570a2db30e02011447f24c55545e444bd5
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-07-25 16:01:04 +0530

    main

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://food-bd69.vercel.app"],
         methods: ["POST","GET"],
         credentials: true
     }
