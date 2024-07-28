8e010e525a29fb7a57cb7bb5e1e2d0a93d384726
Abhijeet Singh <102654858+ajabhijeetsingh@users.noreply.github.com>
2024-07-28 18:06:51 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -29,6 +29,12 @@ app.use(cors(
     }
 ));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://food-bd69.vercel.app"); // Allow your client domain
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 
 //db connection
